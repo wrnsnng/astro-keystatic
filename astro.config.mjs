f95444b695afd7e5f9f2d0b43e2c3f46791130cc
wrnsnng <83326476+wrnsnng@users.noreply.github.com>
2025-03-05 23:04:15 +1100

    adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro'
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
+  adapter: vercel(),
 });
